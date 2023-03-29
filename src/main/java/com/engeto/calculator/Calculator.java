8109f933339170edd1a48b43bb8b5469904a451a
Josef Korba <josef.korba1@gmail.com>
2023-03-29 23:10:59 +0200

    Doplněn kód.

--- src/main/java/com/engeto/calculator/Calculator.java
+++ src/main/java/com/engeto/calculator/Calculator.java
@@ -2,4 +2,41 @@ package com.engeto.calculator;
 
 public class Calculator {
 
+    public int add (int a, int b) {
+        return a + b;
+    }
+
+    public int subtract (int a, int b) {
+        return a - b;
+    }
+
+    public int multiply (int a, int b) {
+        return a * b;
+    }
+
+    public double divide (int a, int b) {
+        if (b==0) {
+            throw new ArithmeticException ("Nelze dělit nulou");
+        }
+        return (double) a / b;
+    }
+
+    public double squareRoot (int a) {
+        if (a<0) {
+            throw new ArithmeticException ("Nejde použít záporné číslo");
+        }
+        return Math.sqrt(a);
+    }
+
+    public double square (int a) {
+        return a * a;
+    }
+
+    public double areaOfCircle (int a) {
+        if (a<=0) {
+            throw new ArithmeticException ("Nejde použít záporné číslo");
+        }
+        return a * a * Math.PI;
+    }
+
 }
